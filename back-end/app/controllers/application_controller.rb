3d7e92401a5b430fb9e7ec559109faba7f96b323
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-03-09 18:01:49 +0300

    added routes

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -74,6 +74,44 @@ class ApplicationController < Sinatra::Base
         AppReview.find(params[:id]).to_json
     end
 
+    get '/reservations' do
+       Reservation.all.to_json
+    end
+    
+    get '/tables' do
+       Table.all.to_json
+    end
+
+    get '/tables/:id' do
+        table = Table.find(params[:id])
+        table.to_json
+    end
+
+    get '/tables/rest/:restaurant_id' do
+        tables = Table.where(restaurant_id: params[:restaurant_id])
+        tables.to_json
+    end
+
+    get '/tables/:table_id/reservations' do
+        table = Table.find(params[:table_id])
+        table.reservations.to_json
+    end
+
+    get '/reservations/:id' do
+        reservation = Reservation.find(params[:id])
+        reservation.to_json
+    end
+
+    get '/reservations/rest/:restaurant_id' do
+        reservations = Reservation.where(restaurant_id: params[:restaurant_id])
+        reservations.to_json
+    end
+
+    get '/reservations/user/:user_id' do
+        reservations = Reservation.where(user_id: params[:user_id])
+        reservations.to_json
+    end
+
     post '/users' do 
         username = params[:username]
         email = params[:email]
@@ -181,6 +219,114 @@ class ApplicationController < Sinatra::Base
             message.to_json
         else
             message = {:error => "Review does not exist"}
+            message.to_json
+        end
+    end
+
+    delete '/reservations/:id' do
+        count_reservations = Reservation.where(id: params[:id]).count()
+        if count_reservations > 0
+            reservation = Reservation.find(params[:id])
+            reservation.destroy
+            message = {:success => "Reservation deleted successfully"}
+            message.to_json
+        else
+            message = {:error => "Reservation does not exist"}
+            message.to_json
+        end
+    end
+
+    delete '/tables/:id' do
+        count_tables = Table.where(id: params[:id]).count()
+        if count_tables > 0
+            table = Table.find(params[:id])
+            table.destroy
+            message = {:success => "Table deleted successfully"}
+            message.to_json
+        else
+            message = {:error => "Table does not exist"}
+            message.to_json
+        end
+    end
+
+    delete '/restaurants/:id' do
+        count_restaurants = Restaurant.where(id: params[:id]).count()
+        if count_restaurants > 0
+            restaurant = Restaurant.find(params[:id])
+            restaurant.destroy
+            message = {:success => "Restaurant deleted successfully"}
+            message.to_json
+        else
+            message = {:error => "Restaurant does not exist"}
+            message.to_json
+        end
+    end
+
+    post '/tables' do 
+        name = params[:name]
+        capacity = params[:capacity]
+        description = params[:description]
+        restaurant_id = params[:restaurant_id]
+
+        if(name.present?)
+            check_table_exists = Table.where(name:name).count()
+            if check_table_exists < 1
+                table = Table.create(name:name, capacity:capacity, description:description, restaurant_id: restaurant_id)
+                if table
+                    message = {:success => "table created successfully"}
+                    message.to_json
+                else 
+                    message = {:error => "Error saving table"}
+                    message.to_json
+                end
+            else
+                message = {:error => "Table already exists"}
+                message.to_json
+            end
+        end
+    end
+
+    post '/restaurants' do 
+        name = params[:name]
+        address = params[:address]
+        image_url = params[:image_url]
+
+        if(name.present? && address.present?)
+            check_restaurant_exists = Restaurant.where(name:name).count()
+            if check_table_exists < 1
+                restaurant = Restaurant.create(name:name, address:address, image_url:image_url)
+                if restaurant
+                    message = {:success => "restaurant created successfully"}
+                    message.to_json
+                else 
+                    message = {:error => "Error saving table"}
+                    message.to_json
+                end
+            else
+                message = {:error => "Restaurant already exists"}
+                message.to_json
+            end
+        end
+    end
+
+    patch '/restaurants/:id' do 
+        id = params[:id]
+        restaurant = Restaurant.find_by(id: id)
+
+        if restaurant
+            name = params[:name] || restaurant.name
+            address = params[:address] || restaurant.address
+            image_url = params[:image_url] || restaurant.image_url
+
+            if restaurant.update(name: name, address: address, image_url: image_url)
+            message = { success: 'Restaurant updated successfully' }
+            message.to_json
+            else 
+            message = { error: 'Error updating restaurant' }
+            message.to_json
+            end
+        else
+            message = { error: 'Restaurant not found' }
             message.to_json
         end
     end
