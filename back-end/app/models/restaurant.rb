ae670b7a0bdfcc6ba6bca2d8db2360836281f12f
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-03-09 11:46:18 +0300

    made model associations

--- back-end/app/models/restaurant.rb
+++ back-end/app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ActiveRecord::Base
     has_many :reviews
     has_many :favorites
+    has_many :reservations
+    has_many :users, through: :reservations
     has_many :users, through: :reviews
     has_many :users_who_favorited, through: :favorites, source: :user
 
--- back-end/app/models/user.rb
+++ back-end/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_many :reviews
+    has_many :reservations
     has_many :favorites
     has_many :app_reviews
     has_many :restaurants, through: :reviews
