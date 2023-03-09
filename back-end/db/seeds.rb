f96b311935e395046531b12cea6b56fac72837fa
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-03-09 12:32:32 +0300

    updated seeds.rb

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -4,8 +4,16 @@ puts "Seeding sample data..."
 50.times do
   User.create(
     username: Faker::Internet.username,
-    email: Faker::Internet.email
-    password: 123456
+    email: Faker::Internet.email,
+    password: '123456'
+  )
+end
+
+# create 25 fake reviews associated with random users
+25.times do
+  AppReview.create(
+    comment: Faker::Lorem.sentence,
+    user_id: rand(User.first.id..User.last.id)
   )
 end
 
@@ -14,6 +22,14 @@ end
   Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address
+  )
+end
+
+# create 100 favorites
+100.times do
+  Favorite.create(
+    user_id: rand(User.first.id..User.last.id),
+    restaurant_id: rand(Restaurant.first.id..Restaurant.last.id)
   )
 end
 
@@ -26,20 +42,26 @@ end
   )
 end
 
-# create 100 favorites
-100.times do
-  Favorite.create(
-    user_id: rand(User.first.id..User.last.id),
-    restaurant_id: rand(Restaurant.first.id..Restaurant.last.id)
+# create 250 tables
+250.times do
+  Table.create(
+    name:  Faker::Lorem.words(number: 2).join(' '),
+    restaurant_id: rand(Restaurant.first.id..Restaurant.last.id),
+    capacity: rand(1..12),
+    description: Faker::Lorem.paragraph
   )
 end
 
-# create 25 fake reviews associated with random users
-25.times do
-  AppReview.create(
-    comment: Faker::Lorem.sentence,
-    star_rating: rand(1..5),
-    user_id: rand(User.first.id..User.last.id)
+#create 1000 reservations
+1000.times do
+  Reservation.create(
+    user_id: rand(User.first.id..User.last.id),
+    restaurant_id: rand(Restaurant.first.id..Restaurant.last.id),
+    table_id: rand(Table.first.id..Table.last.id),
+    num_clients: rand(1..Table.last.capacity),
+    time: Time.now + rand(1..30).hours,
+    date: Date.today + rand(1..30).days,
+    comment: Faker::Lorem.paragraph
   )
 end
 
